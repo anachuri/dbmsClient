0dee50f360504d68bc2ddef2185636a450d0d68a
imaxii <maxigroxx@gmail.com>
2026-01-25 20:26:39 -0300

    agrego cambios

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,8 @@
 #include <QLabel>
 #include "./ui_mainwindow.h"
 //#include "dbtreewidget.h"
+#include <QFileDialog>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -17,10 +19,6 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow() {
     delete ui;
 }
-
-void MainWindow::on_actionOpen_triggered() {}
-
-void MainWindow::on_actionSave_triggered() {}
 
 void MainWindow::on_actionOpen_Sql_triggered() {}
 
@@ -51,11 +49,28 @@ void MainWindow::on_actionPreferences_triggered() {}
 void MainWindow::on_actionManual_triggered() {}
 
 void MainWindow::on_actionAbout_dbmsClient_triggered() {
-    QDialog dialog;
-    dialog.setWindowTitle("About dbmsClient");
-    QHBoxLayout *layout = new QHBoxLayout(&dialog);
-    layout->addWidget(new QLabel("Created by anachuri - January 2026", &dialog));
-    dialog.exec();
+    // QDialog dialog;
+    // dialog.setWindowTitle("About dbmsClient");
+    // QHBoxLayout *layout = new QHBoxLayout(&dialog);
+    // layout->addWidget(new QLabel("Created by anachuri - January 2026")); //&dialog innecesario
+    // dialog.exec();
+
+    QMessageBox::about(this, "dbmsClient", "Created by anachuri - January 2026");
 }
 
 void MainWindow::on_actionFind_and_replace_triggered() {}
+
+void MainWindow::on_actionOpenDatabase_triggered() {
+    QString fileName = QFileDialog::getOpenFileName(this,
+                                                    "Open database",
+                                                    QDir::currentPath(),
+                                                    "database (*.db)");
+    if (fileName.isEmpty())
+        return;
+    QTreeWidgetItem *treeItem = new QTreeWidgetItem();
+    treeItem->setIcon(0, QIcon(":/img/database.png"));
+    treeItem->setText(0, fileName);
+    ui->treeWidget->addTopLevelItem(treeItem);
+}
+
+void MainWindow::on_actionSaveDatabase_triggered() {}
--- mainwindow.h
+++ mainwindow.h
@@ -17,33 +17,20 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 private slots:
-    void on_actionOpen_triggered();
-
-    void on_actionSave_triggered();
-
     void on_actionOpen_Sql_triggered();
-
     void on_actionSave_Sql_triggered();
-
     void on_actionPrint_triggered();
-
     void on_actionClose_triggered();
-
     void on_actionCopy_triggered();
-
     void on_actionCut_triggered();
-
     void on_actionPaste_triggered();
-
     void on_actionExecute_triggered();
-
     void on_actionPreferences_triggered();
-
     void on_actionManual_triggered();
-
     void on_actionAbout_dbmsClient_triggered();
-
     void on_actionFind_and_replace_triggered();
+    void on_actionOpenDatabase_triggered();
+    void on_actionSaveDatabase_triggered();
 
 private:
     Ui::MainWindow *ui;
